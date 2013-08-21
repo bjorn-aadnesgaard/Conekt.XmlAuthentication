6c358625e2bfdc4efdb0b7949710dc46dbff829e
Bjorn Aadnesgaard <bjorn@conekted.com>
2013-08-21 09:55:45 -0400

    Added demo js file.
    Moved demo site related functions from XMLAuthentication.js file.

---
+++ Conekt.XmlAuthentication.Web/Assets/Scripts/Demo.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+	xmldemo.load();
+});
+
+var xmldemo = {
+	load: function () {
+		
+		//Check for hashbang
+		var hash = window.location.hash.replace("#!/", "");
+		if(hash == undefined || hash == "") {
+			xmldemo.start();
+		} else {
+			xmlauth.load(hash);
+		}
+	},
+	start: function () {
+		xmlauth.load('default', function () {
+			$('.readme').html('<div style=\"text-align: center; padding-bottom: 20px;\"><img src=\"/assets/styles/images/loading.gif\" /></div>').load('http://markdown.io/_rawhtml/https://raw.github.com/bjorn-aadnesgaard/Conekt.XmlAuthentication/master/README.md', function () { });
+		});
+	}
+}
