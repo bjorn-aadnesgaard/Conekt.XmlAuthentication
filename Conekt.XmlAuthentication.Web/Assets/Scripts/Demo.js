d1d6eb6172f6e0cab48d4d1dd2f07c9a1a5cf060
Bjorn Aadnesgaard <bjorn@conekted.com>
2013-09-19 13:14:31 -0400

    Added ajax search indexing support.
    Support for search indexing through escape fragments. See
    https://developers.google.com/webmasters/ajax-crawling/docs/getting-started

--- Conekt.XmlAuthentication.Web/Assets/Scripts/Demo.js
+++ Conekt.XmlAuthentication.Web/Assets/Scripts/Demo.js
@@ -1,6 +1,7 @@
 ﻿$(document).ready(function () {
 	xmldemo.load();
 
+	//Check to see if the hash has changed and load new content
 	$(window).bind('hashchange', function (e) {
 		xmldemo.load();
 	});
@@ -8,13 +9,27 @@
 
 var xmldemo = {
 	load: function () {
+
+		//Check for google crawler escape fragment
+		var hasFragment = window.location.href.indexOf('?_escaped_fragment_=/') > 0;
 		
 		//Check for hashbang
-		var hash = window.location.hash.replace("#!/", "");
-		if(hash == undefined || hash == "") {
-			xmldemo.start();
+		var hasHashbang = window.location.href.indexOf('#!/') > 0;
+
+		if (hasFragment) {
+			var qs = xmlauth.parameters();
+			xmlauth.load(qs['_escaped_fragment_'].substr(1));
+
+		} else if (hasHashbang) {
+
+			var hash = window.location.hash.replace('#!/', '');
+			if (hash != undefined && hash != '')
+				xmlauth.load(hash);
+			else
+				xmldemo.start();
+
 		} else {
-			xmlauth.load(hash);
+			xmldemo.start();
 		}
 	},
 	start: function () {
